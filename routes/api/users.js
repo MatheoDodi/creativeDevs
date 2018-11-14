3ef3fc2d631b224e2310e49863ce0f4d14802cec
MatthewDodi <matthew.dodi@gmail.com>
2018-11-13 19:32:03 -0800

    type: feat
    - Created new sign up / sign in/ as well as edit profile routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,17 +6,26 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+// Load Inputy Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 // Load User model
 const User = require('../../models/User');
 
-router.get('/tests', (req, res) => res.json({ msg: 'Users Works' }));
-
 // @route GET api/users/register
-
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Form Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists';
+      return res.status(400).json({ errors });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // Size of avatar
@@ -32,7 +41,9 @@ router.post('/register', (req, res) => {
 
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
+          if (err) {
+            throw err;
+          }
           console.log('test');
           newUser.password = hash;
           newUser
@@ -49,13 +60,21 @@ router.post('/register', (req, res) => {
 // Login User / Return JWT Token
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Form Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
   User.findOne({ email }).then(user => {
     // Check if user exists
     if (!user) {
-      return res.status(404).json({ email: 'User not Found' });
+      errors.email = 'User not Found';
+      return res.status(404).json(errors);
     }
 
     //Check Password
@@ -81,7 +100,8 @@ router.post('/login', (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: 'Password incorrect' });
+        errors.password = 'Password incorrect';
+        return res.status(400).json(errors);
       }
     });
   });
