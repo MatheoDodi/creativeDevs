1cd1752aa46f828f2e26c353d11ea49ead9277bc
MatthewDodi <matthew.dodi@gmail.com>
2018-11-21 10:46:00 -0800

    type: feat - imported react-moment and now dates display correctly

--- client/src/components/dashboard/Experience.js
+++ client/src/components/dashboard/Experience.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import Moment from 'react-moment';
 
 class Experience extends Component {
   render() {
-    const experiece = this.props.exp.map(exp => (
-      <tr key={exp.id}>
+    const { exp } = this.props;
+    const experience = exp.map(exp => (
+      <tr key={exp._id}>
         <td>{exp.company}</td>
         <td>{exp.title}</td>
         <td>
-          {exp.from} - {exp.to}
+          <Moment format="YYYY/MM/DD" date={exp.from} /> -
+          {exp.to ? <Moment format="YYYY/MM/DD" date={exp.to} /> : 'Present'}
         </td>
         <td>
           <button className="btn btn-info">Delete</button>
         </td>
       </tr>
     ));
+    console.log(experience);
     return (
-      <div className>
+      <div>
         <h4 className="mb-4">Experience</h4>
         <table className="table">
           <thead>
@@ -28,7 +32,7 @@ class Experience extends Component {
               <th />
             </tr>
           </thead>
-          <tbody>{experiece}</tbody>
+          <tbody>{experience}</tbody>
         </table>
       </div>
     );
