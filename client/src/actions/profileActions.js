f8463c657124d289a9b6300813666217b1921866
MatthewDodi <matthew.dodi@gmail.com>
2018-11-22 12:34:54 -0800

    type: feat - now Profiles.js gets all the profiles via the appropriate action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -97,6 +97,24 @@ export const handleAddEducation = (newEdu, history) => dispatch => {
     );
 };
 
+export const handleGetAllProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get('api/profile/all')
+    .then(res =>
+      dispatch({
+        type: actionTypes.GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: actionTypes.GET_PROFILES,
+        payload: null
+      })
+    );
+};
+
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {
     axios
