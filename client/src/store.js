5f2671af6e7d900daf1f060fb4ffb63f3afea27e
MatthewDodi <matthew.dodi@gmail.com>
2018-11-25 15:00:01 -0800

    type: trying to fix HEROKU DEPLOYMENT, ugh

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,22 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
+const initialState = {};
+
 const middleware = [thunk];
 
-export const store = createStore(
+// Developer tools middleware
+const composeSetup =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : compose;
+
+const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  initialState,
+  composeSetup(applyMiddleware(...middleware))
 );
+
+export default store;
